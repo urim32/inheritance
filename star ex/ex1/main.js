066a33d5cdbde124db5ee4b89b7970f0ae9bd442
uri malasa <urimalasa30@gmail.com>
2019-06-17 14:16:39 +0300

    added the continue of ex1

--- star ex/ex1/main.js
+++ star ex/ex1/main.js
@@ -5,8 +5,8 @@ class Student {
     }
 }
 class Programmer extends Student {
-    constructor(os, languages, vcs, courses) {
-        super("programmer", courses);
+    constructor(name, os, languages, vcs, courses) {
+        super(name, courses);
         this.os = os;
         this.languages = languages;
         this.vcs = vcs;
@@ -18,5 +18,24 @@ class Programmer extends Student {
 }
 
 
-let programmer1 = new Programmer("win", ["c", "c++", "javascript"], "git", ["infi", "statics", "intro to cs"])
+let programmer1 = new Programmer("uri", "win", ["c", "c++", "javascript"], "git", ["infi", "statics", "intro to cs"]);
+let student1 = new Student("avi", ["cs", "javascript"]);
+let programmer2 = new Programmer("yaakkov", "ios", ["java", "javascript"], "svn", ["infi", "statics", "geographic"]);
+let programmer3 = new Programmer("gilad", "linux", ["c", "c++", "java", "javascript"], "git", ["infi", "statics", "intro to cs"]);
+let programmer4 = new Programmer("tomer", "win", ["c", "c++", "javascript"], "git", ["infi", "statics", "intro to cs"]);
+students = [student1, programmer1, programmer2, programmer3];
 programmer1.isProgramingIn("c#");
+
+function isPrograming(students) {
+    for (let i = 0; i < students.length; i++) {
+
+        if (students[i] instanceof Programmer) {
+            if (students[i].languages.includes("javascript")) {
+                return students[i];
+            }
+
+        }
+
+    }
+}
+isPrograming(students);
